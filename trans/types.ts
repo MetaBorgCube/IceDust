a1eb1f1e18bf27f40e3dd19dda93546424db1994
dcharkes <dc.harkes@gmail.com>
2014-01-24 18:15:02 +0100

    typing for literals (tries for type checking other things -> nothing fruitful yet)

--- trans/types.ts
+++ trans/types.ts
@@ -6,4 +6,10 @@ imports
 
 type rules
 
-
+	
+	//FIXME: attribute value assignment doesn't appear to trigger errors yet.
+	AttributeValue(attributeName, attributeValue) :-
+		where	attributeName	: attributeType
+		and		attributeValue	: valueType
+		and		attributeType == valueType 
+		else error "Wrong type supplied" on attributeValue
