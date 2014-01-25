ec4cadebccd340d45bc476181ebe2b0af985b75d
dcharkes <dc.harkes@gmail.com>
2014-01-25 14:40:03 +0100

    type check constraint for primitive type assignment

--- trans/types.ts
+++ trans/types.ts
@@ -6,10 +6,16 @@ imports
 
 type rules
 
-	
-	//FIXME: attribute value assignment doesn't appear to trigger errors yet.
+	//attribute assignment
 	AttributeValue(attributeName, attributeValue) :-
-		where	attributeName	: attributeType
+		where	attributeName	: attributeType					//FIXME: (#1) doesn't work without the rule in types.str
 		and		attributeValue	: valueType
-		and		attributeType == valueType 
+		and		attributeType == valueType
 		else error "Wrong type supplied" on attributeValue
+		
+	//role assignment
+	RoleValue(roleName, valueEntityName) :-
+		where	roleName		: roleType						//FIXME: (#1)
+		and		valueEntityName	: valueType						//FIXME: (#1)
+		and		roleType == valueType
+		else error "Wrong type supplied" on valueEntityName
