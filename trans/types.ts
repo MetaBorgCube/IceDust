e436ac0c93518efd6ae4d53a8c5b6e674c3c47c9
dcharkes <dc.harkes@gmail.com>
2014-01-25 14:45:20 +0100

    type check for role assignment

--- trans/types.ts
+++ trans/types.ts
@@ -11,11 +11,11 @@ type rules
 		where	attributeName	: attributeType					//FIXME: (#1) doesn't work without the rule in types.str
 		and		attributeValue	: valueType
 		and		attributeType == valueType
-		else error "Wrong type supplied" on attributeValue
+		else error "Wrong type supplied" on attributeValue		//TODO: give expected and given type
 		
 	//role assignment
 	RoleValue(roleName, valueEntityName) :-
 		where	roleName		: roleType						//FIXME: (#1)
 		and		valueEntityName	: valueType						//FIXME: (#1)
 		and		roleType == valueType
-		else error "Wrong type supplied" on valueEntityName
+		else error "Wrong type supplied" on valueEntityName		//TODO: give expected and given type
