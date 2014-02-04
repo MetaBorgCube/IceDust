34fbf759218868e116698ce526e990e0b761bd2e
dcharkes <dc.harkes@gmail.com>
2014-02-04 22:14:14 +0100

    added arithmetic expressions

--- relations/trans/types.ts
+++ relations/trans/types.ts
@@ -4,18 +4,18 @@ imports
 
 
 
-type rules
+type rules // constraints
 
 	//attribute assignment
 	AttributeValue(attributeName, attributeValue) :-
-		where	attributeName	: attributeType					//FIXME: (#1) doesn't work without the rule in types.str
+		where	attributeName		: attributeType					//FIXME: (#1) doesn't work without the rule in types.str
 		and		attributeValue	: valueType
 		and		attributeType == valueType
 		else error "Wrong type supplied" on attributeValue		//TODO: give expected and given type
 		
 	//role assignment
 	RoleValue(roleName, valueEntityName) :-
-		where	roleName		: roleType						//FIXME: (#1)
+		where	roleName				: roleType						//FIXME: (#1)
 		and		valueEntityName	: valueType						//FIXME: (#1)
 		and		roleType == valueType
 		else error "Wrong type supplied" on valueEntityName		//TODO: give expected and given type
@@ -26,3 +26,10 @@ type rules
 		and		expressionType == type
 		else error "Wrong type supplied" on expression
 
+type rules // expressions
+	BinExp(operator, exp1, exp2) : exp2type
+		where	exp1		: exp1type
+		and		exp2		: exp2type
+		and		exp1type == exp2type
+		else error "Not the same types supplied to the Binary Operator." on exp2
+		
