1ceffde22c31496fed07a391a7ffe0853318f62f
dcharkes <dc.harkes@gmail.com>
2014-03-08 00:06:03 +0100

    try typing functions

--- relations/trans/typing/aggregation.ts
+++ relations/trans/typing/aggregation.ts
@@ -26,13 +26,26 @@ type rules
 	and (
 				x-mu == ZeroOrMore() //and ZeroOrOne() => mu
 		 or x-mu == OneOrMore()	 //and One() => mu
-		 or x-mu == ZeroOrOne()	 //and ZeroOrOne() => mu
-		 or x-mu == One()				 //and One() => mu
+		 or x-mu == ZeroOrOne()	 //and ZeroOrOne() => mu // --> this should give an error or warning
+		 or x-mu == One()				 //and One() => mu  		 // --> this should give an error or warning
 	) 
 	// else error "Expected multiplicity of higher than One" on x
+		
+// 	Min(x)
+// +	Max(x)
+// +	Avg(x) has multiplicity mu-single
+// 	where x has multiplicity x-mu
+// 	  and <mu-to-single> (x-mu) => mu-single
 		
 	Concat(x) : x-ty
 	where x : x-ty
 		and x-ty == String() else error "String expected" on x
 		
 	Concat(x) has multiplicity One() //TODO: zero goes to multiplicity zero or to empty string?
+
+type functions
+
+	// mu-to-single :
+	// 	mu -> single-mu
+	// 	where ((mu == ZeroOrMore() or mu == ZeroOrOne()) and ZeroOrOne() => single-mu)
+	// 	   or ((mu == OneOrMore()	 or mu == One())			 and One() 			 => single-mu)
