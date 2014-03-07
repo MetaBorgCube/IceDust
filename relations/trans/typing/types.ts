058b2b8f9ab15729d3c3974ebeb2e2d50239f49e
dcharkes <dc.harkes@gmail.com>
2014-03-07 11:49:24 +0100

    type check Ints and Strings differently

--- relations/trans/typing/types.ts
+++ relations/trans/typing/types.ts
@@ -61,14 +61,20 @@ type rules // constraints: attributes & values
 
 type rules // constraints: expressions
 	
-	Multiplication(x, y)
-+	Division(x, y)
-+	Modulo(x, y)
-+	Addition(x, y)
-+	Subtraction(x, y) : y-ty
+
+	Addition(x, y) : y-ty
 	where	x	: x-ty
 		and	y	: y-ty
 		and	x-ty == y-ty else error "Not the same types supplied to Addition." on y
+		
+	Multiplication(x, y)
++	Division(x, y)
++	Modulo(x, y)
++	Subtraction(x, y) : y-ty
+	where	x	: x-ty
+		and	y	: y-ty
+		and	x-ty == Int() else error "Expected Int" on x
+		and	y-ty == Int() else error "Expected Int" on y
 		
 	Multiplication(x, y)
 +	Division(x, y)
@@ -77,7 +83,7 @@ type rules // constraints: expressions
 +	Subtraction(x, y) has multiplicity y-mu
 	where	x	has multiplicity x-mu
 		and	y	has multiplicity y-mu
-		and	x-mu == y-mu else error "Not the same multiplicities supplied to Addition." on y
+		and	x-mu == y-mu else error "Not the same multiplicities supplied to Binary Expression." on y
 		
 	Aggregation(op, x) : x-ty
 		where	x	: x-ty
