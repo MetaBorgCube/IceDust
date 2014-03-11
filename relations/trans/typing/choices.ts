8462c3b9faa379909895032fa2aa51a13d8baf4b
dcharkes <dc.harkes@gmail.com>
2014-03-11 11:58:13 +0100

    multiplicity merge operator

--- relations/trans/typing/choices.ts
+++ relations/trans/typing/choices.ts
@@ -20,3 +20,12 @@ type rules
 	ChoiceLeft(x,y) has multiplicity One()	//TODO: both the min and max should me upped to the maximum of the options
 	where	x	: x-mu
 		and	y	: y-mu
+		
+	Merge(x,y) : x-ty
+	where	x	: x-ty
+		and	y	: y-ty
+		and	x-ty == y-ty else error "Expected the same type on the right side as on the left side on the Merge." on y
+		
+	Merge(x,y) has multiplicity OneOrMore()	//TODO: both the min and max should me upped to the maximum of the options
+	where	x	: x-mu
+		and	y	: y-mu
