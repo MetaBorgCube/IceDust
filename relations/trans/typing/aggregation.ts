fff8d8a711348ecc97c2a461133464bc8f45e8cd
dcharkes <dc.harkes@gmail.com>
2014-03-13 11:41:14 +0100

    multiplicities: typing functions for math and aggregations

--- relations/trans/typing/aggregation.ts
+++ relations/trans/typing/aggregation.ts
@@ -19,20 +19,23 @@ type rules
 	where	x	: x-ty
 		and x-ty == Int() else error $[Type mismatch: expected Int got [x-ty] in Aggregation] on x
 
-	Min(x)
-+	Max(x)
-+	Avg(x) has multiplicity mu
-	where x has multiplicity x-mu
-	and (
-				x-mu == ZeroOrMore() and ZeroOrOne() => mu
-		 or x-mu == OneOrMore()	 and One() 			 => mu
-		 or x-mu == ZeroOrOne()	 and ZeroOrOne() => mu // --> this should give an error or warning
-		 or x-mu == One()				 and One()			 => mu // --> this should give an error or warning
-	) 
-	else error "Expected multiplicity of higher than One" on x
-		
 	Concat(x) : x-ty
 	where x : x-ty
 		and x-ty == String() else error $[Type mismatch: expected String got [x-ty] in Concatenation] on x
-		
-	Concat(x) has multiplicity One() //TODO: zero goes to multiplicity zero or to empty string?
+
+
+	Min(x)
++	Max(x)
++	Avg(x)
++ Concat(x) has multiplicity mu
+	where x has multiplicity x-mu
+		and <mu-aggr> (x-mu) => mu
+		and (x-mu == ZeroOrMore() or x-mu == OneOrMore())	else error "Expected multiplicity of higher than One" on x
+
+
+type functions
+
+	mu-aggr:
+		(x-mu) -> mu
+		where ((x-mu == OneOrMore() or x-mu == One()) and One() => mu)
+			 or ZeroOrOne() => mu
--- relations/trans/typing/math.ts
+++ relations/trans/typing/math.ts
@@ -31,7 +31,26 @@ type rules
 +	Division(x, y)
 +	Modulo(x, y)
 +	Addition(x, y)
-+	Subtraction(x, y) has multiplicity y-mu
++	Subtraction(x, y) has multiplicity mu
 	where	x	has multiplicity x-mu
 		and	y	has multiplicity y-mu
-		and	x-mu == y-mu else error $[Multiplicity mismatch: expected [x-mu] got [y-mu] in Math Operation] on y
+		and <mu-or-join> (x-mu, y-mu) => mu
+		and (x-mu == ZeroOrOne() or x-mu == One()) else error $[Multiplicity mismatch: expected One or ZeroOrOne got [x-mu] in Math Operation] on x
+		and (y-mu == ZeroOrOne() or y-mu == One()) else error $[Multiplicity mismatch: expected One or ZeroOrOne got [y-mu] in Math Operation] on y
+
+
+type functions
+
+	mu-or-join:
+		(x-mu, y-mu) -> mu
+		where x-mu == One() and y-mu == One()																										and One() => mu
+			 or (x-mu == ZeroOrOne() or x-mu == One()) and (y-mu == ZeroOrOne() or y-mu == One()) and ZeroOrOne() => mu
+			 or (x-mu == ZeroOrMore() or y-mu == ZeroOrMore())																		and ZeroOrMore() => mu
+			 or x-mu == OneOrMore() and y-mu == ZeroOrOne()																				and ZeroOrMore() => mu
+			 or y-mu == OneOrMore() and x-mu == ZeroOrOne()																				and ZeroOrMore() => mu
+			 or																																												OneOrMore() => mu
+
+		
+		
+		// where ((x-mu == OneOrMore() or x-mu == One()) and One() => mu)
+		// 	 or ZeroOrOne() => mu
