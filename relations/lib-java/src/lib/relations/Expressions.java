795095e1dd02b95d29fe76b3a72e8ff67f8713d3
dcharkes <dc.harkes@gmail.com>
2014-03-19 11:49:27 +0100

    more java-libs

--- relations/lib-java/src/lib/relations/Expressions.java
+++ relations/lib-java/src/lib/relations/Expressions.java
@@ -4,26 +4,45 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class Expressions {
-	  public static <E> Collection<E> toCollection(E e)
-	  { 
-	    Collection<E> collection = new HashSet<E>();
-	    collection.add(e);
-	    return collection;
-	  }
-	  
-	  public static <E> E choice(E e1, E e2){
-		  return e1!=null?e1:e2;
-	  }
-	  
-	  public static <E> Collection<E> choice(Collection<E> e1, E e2){
-		  return e1!=null&&e1.size()>0?e1:toCollection(e2);
-	  }
-	  
-	  public static <E> Collection<E> choice(E e1, Collection<E> e2){
-		  return e1!=null?toCollection(e1):e2;
-	  }
-	  
-	  public static <E> Collection<E> choice(Collection<E> e1, Collection<E> e2){
-		  return e1!=null&&e1.size()>0?e1:e2;
-	  }
+	public static <E> Collection<E> toCollection(E e) {
+		Collection<E> collection = new HashSet<E>();
+		collection.add(e);
+		return collection;
+	}
+
+	public static <E> E choice(E e1, E e2) {
+		return e1 != null ? e1 : e2;
+	}
+
+	public static <E> Collection<E> choice(E e1, Collection<E> e2) {
+		return e1 != null ? toCollection(e1) : e2;
+	}
+	
+	public static <E> Collection<E> choice(Collection<E> e1, E e2) {
+		return e1 != null && e1.size() > 0 ? e1 : toCollection(e2);
+	}
+
+	public static <E> Collection<E> choice(Collection<E> e1, Collection<E> e2) {
+		return e1 != null && e1.size() > 0 ? e1 : e2;
+	}
+
+	public static Integer plus(Integer i, Integer j) {
+		return i != null && j != null ? i + j : null;
+	}
+	
+	public static Integer minus(Integer i, Integer j){
+		return i != null && j != null ? i - j : null;
+	}
+	
+	public static Integer mul(Integer i, Integer j){
+		return i != null && j != null ? i * j : null;
+	}
+	
+	public static Integer mod(Integer i, Integer j){
+		return i != null && j != null && j != 0 ? i % j : null;
+	}
+	
+	public static Integer div(Integer i, Integer j){
+		return i != null && j != null && j != 0 ? i / j : null;
+	}
 }
--- relations/lib-java/test/lib/relations/ExpressionsTest.java
+++ relations/lib-java/test/lib/relations/ExpressionsTest.java
@@ -2,6 +2,8 @@ package lib.relations;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 public class ExpressionsTest {
@@ -9,6 +11,23 @@ public class ExpressionsTest {
 	@Test
 	public void testToCollection() {
 		assertEquals(Expressions.toCollection(4).size(), 1);
+	}
+	
+	@Test
+	public void testChoice() {
+		Integer nullInt = null;
+		Collection<Integer> nullCol = null;
+		assertEquals(new Integer(4), Expressions.choice(4, 3));
+		assertEquals(new Integer(3), Expressions.choice(nullInt, 3));
+		assertEquals(null, Expressions.choice(nullInt, nullInt));
+	}
+	
+	@Test
+	public void testDiv() {
+		assertEquals(3, Expressions.div(6, 2).intValue());
+		assertEquals(null, Expressions.div(null, 2));
+		assertEquals(null, Expressions.div(6, null));
+		assertEquals(null, Expressions.div(6, 0));
 	}
 
 }
