338119d11c533091bbd726416afd8ed7c0f1d5da
dcharkes <dc.harkes@gmail.com>
2014-03-19 14:40:23 +0100

    java libraries updated

--- relations/lib-java/src/lib/relations/Expressions.java
+++ relations/lib-java/src/lib/relations/Expressions.java
@@ -4,11 +4,20 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class Expressions {
+
+	// helper methods
+
+	public static <E> Collection<E> emptyCollection() {
+		return new HashSet<E>();
+	}
+
 	public static <E> Collection<E> toCollection(E e) {
-		Collection<E> collection = new HashSet<E>();
+		Collection<E> collection = emptyCollection();
 		collection.add(e);
 		return collection;
 	}
+
+	// multiplicity expressions
 
 	public static <E> E choice(E e1, E e2) {
 		return e1 != null ? e1 : e2;
@@ -17,32 +26,72 @@ public class Expressions {
 	public static <E> Collection<E> choice(E e1, Collection<E> e2) {
 		return e1 != null ? toCollection(e1) : e2;
 	}
-	
+
 	public static <E> Collection<E> choice(Collection<E> e1, E e2) {
-		return e1 != null && e1.size() > 0 ? e1 : toCollection(e2);
+		return e1.size() > 0 ? e1 : e2 != null ? toCollection(e2) : e1;
 	}
 
 	public static <E> Collection<E> choice(Collection<E> e1, Collection<E> e2) {
-		return e1 != null && e1.size() > 0 ? e1 : e2;
+		return e1.size() > 0 ? e1 : e2;
+	}
+
+	public static <E> Collection<E> merge(E e1, E e2) {
+		Collection<E> c = emptyCollection();
+		if (e1 != null)
+			c.add(e1);
+		if (e2 != null)
+			c.add(e2);
+		return c;
+	}
+
+	public static <E> Collection<E> merge(E e1, Collection<E> e2) {
+		Collection<E> c = emptyCollection();
+		if (e1 != null)
+			c.add(e1);
+		c.addAll(e2);
+		return c;
+	}
+
+	public static <E> Collection<E> merge(Collection<E> e1, E e2) {
+		Collection<E> c = emptyCollection();
+		c.addAll(e1);
+		if (e2 != null)
+			c.add(e2);
+		return c;
+	}
+
+	public static <E> Collection<E> merge(Collection<E> e1, Collection<E> e2) {
+		Collection<E> c = emptyCollection();
+		c.addAll(e1);
+		c.addAll(e2);
+		return c;
+	}
+
+	// math expressions
+	
+	public static String plus(String i, String j) {
+		return i != null && j != null ? i + j : null;
 	}
 
 	public static Integer plus(Integer i, Integer j) {
 		return i != null && j != null ? i + j : null;
 	}
-	
-	public static Integer minus(Integer i, Integer j){
+
+	public static Integer minus(Integer i, Integer j) {
 		return i != null && j != null ? i - j : null;
 	}
-	
-	public static Integer mul(Integer i, Integer j){
+
+	public static Integer mul(Integer i, Integer j) {
 		return i != null && j != null ? i * j : null;
 	}
-	
-	public static Integer mod(Integer i, Integer j){
+
+	public static Integer mod(Integer i, Integer j) {
 		return i != null && j != null && j != 0 ? i % j : null;
 	}
-	
-	public static Integer div(Integer i, Integer j){
+
+	public static Integer div(Integer i, Integer j) {
 		return i != null && j != null && j != 0 ? i / j : null;
 	}
+	
+	// TODO: aggregation expressions
 }
--- relations/lib-java/test/lib/relations/ExpressionsTest.java
+++ relations/lib-java/test/lib/relations/ExpressionsTest.java
@@ -9,17 +9,53 @@ import org.junit.Test;
 public class ExpressionsTest {
 
 	@Test
+	public void testEmptyCollection() {
+		assertEquals(0, Expressions.emptyCollection().size());
+	}
+	
+	@Test
 	public void testToCollection() {
 		assertEquals(Expressions.toCollection(4).size(), 1);
 	}
 	
 	@Test
 	public void testChoice() {
-		Integer nullInt = null;
-		Collection<Integer> nullCol = null;
+		Integer singleMultZero = null;
+		Collection<Integer> manyMultZero = Expressions.emptyCollection();
+		
 		assertEquals(new Integer(4), Expressions.choice(4, 3));
-		assertEquals(new Integer(3), Expressions.choice(nullInt, 3));
-		assertEquals(null, Expressions.choice(nullInt, nullInt));
+		assertEquals(new Integer(3), Expressions.choice(singleMultZero, 3));
+		assertEquals(singleMultZero, Expressions.choice(singleMultZero, singleMultZero));
+		
+		assertEquals(Expressions.toCollection(new Integer(5)), Expressions.choice(manyMultZero, 5));
+		assertEquals(0, Expressions.choice(manyMultZero, singleMultZero).size());
+		
+		assertEquals(Expressions.toCollection(new Integer(6)), Expressions.choice(6, manyMultZero));
+		assertEquals(0, Expressions.choice(singleMultZero, manyMultZero).size());
+		
+		assertEquals(0, Expressions.choice(manyMultZero, manyMultZero).size());
+		assertEquals(Expressions.toCollection(new Integer(7)), Expressions.choice(Expressions.toCollection(new Integer(7)), manyMultZero));
+		assertEquals(Expressions.toCollection(new Integer(8)), Expressions.choice(manyMultZero, Expressions.toCollection(new Integer(8))));
+	}
+	
+	@Test
+	public void testMerge() {
+		Integer singleMultZero = null;
+		Collection<Integer> manyMultZero = Expressions.emptyCollection();
+		
+		assertEquals(2, Expressions.merge(4, 3).size());
+		assertEquals(1, Expressions.merge(singleMultZero, 3).size());
+		assertEquals(0, Expressions.merge(singleMultZero, singleMultZero).size());
+		
+		assertEquals(Expressions.toCollection(new Integer(5)), Expressions.merge(manyMultZero, 5));
+		assertEquals(0, Expressions.merge(manyMultZero, singleMultZero).size());
+		
+		assertEquals(Expressions.toCollection(new Integer(6)), Expressions.merge(6, manyMultZero));
+		assertEquals(0, Expressions.merge(singleMultZero, manyMultZero).size());
+		
+		assertEquals(0, Expressions.merge(manyMultZero, manyMultZero).size());
+		assertEquals(Expressions.toCollection(new Integer(7)), Expressions.merge(Expressions.toCollection(new Integer(7)), manyMultZero));
+		assertEquals(Expressions.toCollection(new Integer(8)), Expressions.merge(manyMultZero, Expressions.toCollection(new Integer(8))));
 	}
 	
 	@Test
