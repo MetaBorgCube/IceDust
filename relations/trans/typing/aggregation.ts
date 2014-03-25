073be25a3000cba8bb6ed4918af4df954d684178
dcharkes <dc.harkes@gmail.com>
2014-03-25 13:18:30 +0100

    added aggregations sum and count (type system and code generation)

--- relations/trans/typing/aggregation.ts
+++ relations/trans/typing/aggregation.ts
@@ -15,6 +15,7 @@ type rules
 
 	Min(x)
 +	Max(x)
++ Sum(x)
 +	Avg(x) : x-ty
 	where	x	: x-ty
 		and x-ty == Int() else error $[Type mismatch: expected Int got [x-ty] in Aggregation] on x
@@ -23,15 +24,19 @@ type rules
 	where x : x-ty
 		and x-ty == String() else error $[Type mismatch: expected String got [x-ty] in Concatenation] on x
 
+	Count(x) : Int()
+
 
 	Min(x)
 +	Max(x)
++ Sum(x)
 +	Avg(x)
 + Concat(x) has multiplicity mu
 	where x has multiplicity x-mu
 		and <mu-aggr> (x-mu) => mu
 		and (x-mu == ZeroOrMore() or x-mu == OneOrMore())	else error "Expected multiplicity of higher than One" on x
 
+	Count(x) has multiplicity One()
 
 type functions
 
