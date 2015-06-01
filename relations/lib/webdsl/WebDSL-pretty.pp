9f5265c82a8ca03b896cf1ec1e1b43310454a547
Daco Harkes <dc.harkes@gmail.com>
2015-06-01 18:27:47 +0200

    to-webdsl: enable parenthesize for webdsl

--- relations/lib/webdsl/WebDSL-pretty.pp
+++ relations/lib/webdsl/WebDSL-pretty.pp
@@ -211,6 +211,7 @@
    Not                                -- KW["!"] _1,
    IsA                                -- H hs=1[_1 KW["is"] KW["a"] _2],
    Cast                               -- H[ KW["("] _1 KW["as"] _2 KW[")"] ],
+   Parenthetical                      -- H hs=0 ["(" _1 ")"],
 
     %%access control syntax
    ValidationAnnoLength                         -- KW["length"] KW["("] _1 KW[","] _2 KW[")"],
@@ -705,6 +706,9 @@
    XMLElement.3:iter-star                       -- _1,
    XMLEmptyElement                              -- KW["<"] _1 _2 KW["/>"],
    XMLEmptyElement.2:iter-star                  -- _1,
+   XMLEmptyElementUnclosed                      -- KW["<"] _1 _2 KW[">"],
+   XMLEmptyElementUnclosed.2:iter-star          -- _1,
+   XMLComment                                   -- _1,
 
 %% Search
    SearchableAnno                               -- _1 KW["*"] _2,
