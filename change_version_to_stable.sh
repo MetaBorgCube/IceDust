5699d1c9fa87e8c4807a841b18dd4e65b3227550
Daco Harkes <dc.harkes@gmail.com>
2017-01-17 16:07:25 +0100

    change IceDust version from 5.0.0-SNAPSHOT to 5.1.0 script

--- change_version_to_stable.sh
+++ change_version_to_stable.sh
@@ -1,2 +1,2 @@
-grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "0.5.0.qualifier" * | xargs sed -i "" "s/0.5.0.qualifier/0.5.0/g"
-grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "0.5.0-SNAPSHOT" * | xargs sed -i "" "s/0.5.0-SNAPSHOT/0.5.0/g"
+grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "0.5.0.qualifier" * | xargs sed -i "" "s/0.5.0.qualifier/0.5.1/g"
+grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "0.5.0-SNAPSHOT" * | xargs sed -i "" "s/0.5.0-SNAPSHOT/0.5.1/g"
