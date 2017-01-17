693f3ab3d8e5189eb6da99f1960a92964609dbdb
Daco Harkes <dc.harkes@gmail.com>
2017-01-17 16:44:19 +0100

    change Spoofax version to 2.2.0-SNAPSHOT script

--- change_to_spoofax_nightly.sh
+++ change_to_spoofax_nightly.sh
@@ -1,2 +1,2 @@
-grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml} "http://download.spoofax.org/update/release/2.0.0/eclipse/site/" search . | xargs sed -i "" "s/http:\/\/download.spoofax.org\/update\/release\/2.0.0\/eclipse\/site\//http:\/\/download.spoofax.org\/update\/nightly\//g"
-grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml} "2.0.0" search . | xargs sed -i "" "s/2.0.0/2.1.0-SNAPSHOT/g"
+grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml} "http://tinyurl.com/spoofax-eclipse-2-1-0" search . | xargs sed -i "" "s/http:\/\/tinyurl.com\/spoofax-eclipse-2-1-0/http:\/\/download.spoofax.org\/update\/nightly\//g"
+grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml} "2.1.0" search . | xargs sed -i "" "s/2.1.0/2.2.0-SNAPSHOT/g"
