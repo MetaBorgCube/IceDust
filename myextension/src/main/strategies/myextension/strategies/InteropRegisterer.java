a8f5f0396914680352c25b47e6ab6a7cb28c318e
Daco Harkes <dc.harkes@gmail.com>
2017-04-24 14:11:25 +0200

    language extension for IceDust language project

---
+++ myextension/src/main/strategies/myextension/strategies/InteropRegisterer.java
@@ -0,0 +1,10 @@
+package myextension.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        super(new Strategy[] { graph_topological_sort_1_0.instance });
+    }
+}
---
+++ myextension/src/main/strategies/myextension/strategies/Main.java
@@ -0,0 +1,9 @@
+package myextension.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
