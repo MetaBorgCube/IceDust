4545274c951518986f074d14ed0fa1704f671548
Daco Harkes <dc.harkes@gmail.com>
2017-05-01 18:38:47 +0200

    change name script (fix)

--- change_name.sh
+++ change_name.sh
@@ -8,5 +8,5 @@ grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --exclude=*change_name.sh* 
 find . -depth -name "*${FROMNAME}*" -execdir sh -c "mv {} \$(echo {} | sed \"s/${FROMNAME}/${TONAME}/\")" \;
 
 # change extension
-grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --exclude=*change_name.sh* --exclude=*_subtyping.ts* --exclude=*_runtime.str* "extensions: ${FROMEXTENSION}" * | xargs sed -i "" "s/extensions: ${FROMEXTENSION}/extensions: ${TOEXTENSION}/g"
+grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --exclude=*change_name.sh* --exclude=*_subtyping.ts* --exclude=*_runtime.str* "extensions : ${FROMEXTENSION}" * | xargs sed -i "" "s/extensions : ${FROMEXTENSION}/extensions : ${TOEXTENSION}/g"
 find . -depth -name "*.${FROMEXTENSION}*" -execdir sh -c "mv {} \$(echo {} | sed \"s/.${FROMEXTENSION}/.${TOEXTENSION}/\")" \;
