36bff7a1078d963ab672cc51e62d0cb37596c8b1
Daco Harkes <dc.harkes@gmail.com>
2017-09-25 11:54:06 +0200

    WebDSL backend: (started with) make eventual concurrent, thread pool for calculating derived value tasks

--- icedust/lib-java/src/derivations/Settings.java
+++ icedust/lib-java/src/derivations/Settings.java
@@ -1,5 +1,15 @@
 package derivations;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import utils.AbstractPageServlet;
+import utils.GlobalVariables;
+import utils.GlobalsPageServlet;
+import utils.ThreadLocalOut;
+import utils.ThreadLocalPage;
+
 public class Settings {
 
     static volatile boolean updatesEnabled = true;
@@ -11,5 +21,55 @@ public class Settings {
     public static void setUpdatesEnabled(boolean setting){
         updatesEnabled = setting;
     }
+    
+	private static ScheduledExecutorService ex;
+	
+	public static void initTimers(int n, int millis){
+		ex = Executors.newScheduledThreadPool(n);
+		
+		java.util.TimerTask timer69 = new java.util.TimerTask() {
+			public void run() {
+				if (utils.GlobalVariables.globalvarsChecked && utils.GlobalInit.initChecked) {
+					org.hibernate.Session hibSession = null;
+					try {
+						org.webdsl.servlet.ServletState
+								.scheduledTaskStarted("invoke updateDerivationsAsync() every x milliseconds with y threads");
+						AbstractPageServlet ps = new GlobalsPageServlet();
+						ThreadLocalPage.set(ps);
+						ps.initRequestVars();
+						hibSession = utils.HibernateUtil.getCurrentSession();
+						hibSession.beginTransaction();
+						if (GlobalVariables.initGlobalVars(ps.envGlobalAndSession,
+								utils.HibernateUtil.getCurrentSession())) {
+							java.io.PrintWriter out = new java.io.PrintWriter(System.out);
+							ThreadLocalOut.push(out);
+							webdsl.generated.functions.updateDerivationsAsync_.updateDerivationsAsync_();
+							utils.HibernateUtil.getCurrentSession().getTransaction().commit();
+							ThreadLocalOut.popChecked(out);
+							ps.invalidatePageCacheIfNeeded();
+						}
+					} catch (Exception ex) {
+						org.webdsl.logging.Logger.error("exception occured while executing timed function: "
+								+ "invoke updateDerivationsAsync() every x milliseconds");
+						org.webdsl.logging.Logger.error("exception message: " + ex.getMessage(), ex);
+						utils.HibernateUtil.getCurrentSession().getTransaction().rollback();
+					} finally {
+						org.webdsl.servlet.ServletState.scheduledTaskEnded();
+						ThreadLocalPage.set(null);
+					}
+				}
+			}
+		};
+		
+		for(int i = 0; i<n; i++){
+			ex.scheduleAtFixedRate(timer69, 0 + n, millis + 0, TimeUnit.MILLISECONDS);
+		}
+	}
+	
+	public static void ensureTimers(int n, int millis){
+		if(ex==null){
+			initTimers(n, millis);
+		}
+	}
     
 }
