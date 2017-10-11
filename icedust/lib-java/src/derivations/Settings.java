ddf7594f9dc38fe69c6f13dfce08cc8c15721b19
Daco Harkes <dc.harkes@gmail.com>
2017-10-11 16:10:45 +0200

    WebDSL backend: make number of eventual threads configurable (1/2)

--- icedust/lib-java/src/derivations/Settings.java
+++ icedust/lib-java/src/derivations/Settings.java
@@ -1,10 +1,13 @@
 package derivations;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import utils.AbstractPageServlet;
@@ -25,72 +28,15 @@ public class Settings {
         updatesEnabled = setting;
     }
 
+    static WorkerSet workers;
+
     public Settings(int n, int millis) {
-        initTimers(n, millis);
+        workers = new WorkerSet(millis);
+        workers.setNumWorkers(n);
     }
 
-    private static ScheduledExecutorService ex;
-
-    public static void initTimers(int n, int millis) {
-        ex = Executors.newScheduledThreadPool(n);
-
-        java.util.TimerTask timer69 = new java.util.TimerTask() {
-            public void run() {
-                Thread thisThread = Thread.currentThread();
-
-                while (DirtyCollections.getI() < 2147483647 && updatesEnabled) {
-                    if (utils.GlobalVariables.globalvarsChecked && utils.GlobalInit.initChecked) {
-                        org.hibernate.Session hibSession = null;
-                        try {
-                            org.webdsl.servlet.ServletState.scheduledTaskStarted(
-                                    "invoke updateDerivationsAsync() every x milliseconds with y threads");
-                            AbstractPageServlet ps = new GlobalsPageServlet();
-                            ThreadLocalPage.set(ps);
-                            ps.initRequestVars();
-                            hibSession = utils.HibernateUtil.getCurrentSession();
-                            hibSession.beginTransaction();
-                            if (GlobalVariables.initGlobalVars(ps.envGlobalAndSession,
-                                    utils.HibernateUtil.getCurrentSession())) {
-                                java.io.PrintWriter out = new java.io.PrintWriter(System.out);
-                                ThreadLocalOut.push(out);
-                                webdsl.generated.functions.updateDerivationsAsyncThread_
-                                        .updateDerivationsAsyncThread_(thisThread);
-                                utils.HibernateUtil.getCurrentSession().getTransaction().commit();
-                                ThreadLocalOut.popChecked(out);
-                                ps.invalidatePageCacheIfNeeded();
-                            }
-                        } catch (org.hibernate.StaleStateException
-                                | org.hibernate.exception.LockAcquisitionException ex) {
-                            org.webdsl.logging.Logger
-                                    .error("updateDerivationsAsync() database collision, rescheduling");
-
-                            reschedule(thisThread);
-
-                            utils.HibernateUtil.getCurrentSession().getTransaction().rollback();
-                        } catch (Exception ex) {
-                            org.webdsl.logging.Logger.error(ex.getClass().getCanonicalName());
-                            org.webdsl.logging.Logger.error("exception occured while executing timed function: "
-                                    + "invoke updateDerivationsAsync() every x milliseconds");
-                            org.webdsl.logging.Logger.error("exception message: " + ex.getMessage(), ex);
-                            utils.HibernateUtil.getCurrentSession().getTransaction().rollback();
-                        } finally {
-                            org.webdsl.servlet.ServletState.scheduledTaskEnded();
-                            ThreadLocalPage.set(null);
-                        }
-                    }
-                }
-            }
-        };
-
-        for (int i = 0; i < n; i++) {
-            ex.scheduleWithFixedDelay(timer69, 0 + n, millis + 0, TimeUnit.MILLISECONDS);
-        }
-    }
-
-    public static void ensureTimers(int n, int millis) {
-        if (ex == null) {
-            initTimers(n, millis);
-        }
+    public void setNumWorkers(int n) {
+        workers.setNumWorkers(n);
     }
 
     public static boolean getLogincremental() {
@@ -132,4 +78,108 @@ public class Settings {
         DirtyCollections.dirtyI(0);
     }
 
+}
+
+class WorkerSet {
+
+    private int numWorkers = 0;
+    private int checkInterval;
+    private ScheduledExecutorService ex = Executors.newScheduledThreadPool(16); // is maxThreads
+    private ArrayList<ScheduledFuture<?>> schedules = new ArrayList<ScheduledFuture<?>>();
+
+    public WorkerSet(int checkInterval) {
+        this.checkInterval = checkInterval;
+    }
+
+    public void setNumWorkers(int numWorkers) {
+        int oldNumWorkers = schedules.size();
+        this.numWorkers = numWorkers;
+        if (numWorkers > oldNumWorkers) {
+            for (int i = oldNumWorkers; i < numWorkers; i++) {
+                addWorker(i);
+            }
+        }
+        if (numWorkers < oldNumWorkers) {
+            for (int i = oldNumWorkers - 1; i >= numWorkers; i--) {
+                removeWorker(i);
+            }
+        }
+    }
+
+    private void addWorker(int index) {
+        if (schedules.size() <= index) {
+            System.out.println("Adding Worker " + index);
+            TimerTask task = newTask(index);
+            ScheduledFuture<?> schedule = ex.scheduleWithFixedDelay(task, 0, checkInterval, TimeUnit.MILLISECONDS);
+            schedules.add(index, schedule);
+        }
+    }
+
+    private TimerTask newTask(final int index) {
+        return new java.util.TimerTask() {
+            public void run() {
+                Thread thisThread = Thread.currentThread();
+
+                while (DirtyCollections.getI() < 2147483647 && Settings.getUpdatesEnabled()) {
+                    if (numWorkers <= index) {
+                        System.out.println("Worker " + index + ": shutting down");
+                        break;
+                    }
+
+                    if (utils.GlobalVariables.globalvarsChecked && utils.GlobalInit.initChecked) {
+                        org.hibernate.Session hibSession = null;
+                        try {
+                            org.webdsl.servlet.ServletState.scheduledTaskStarted(
+                                    "invoke updateDerivationsAsync() every x milliseconds with y threads");
+                            AbstractPageServlet ps = new GlobalsPageServlet();
+                            ThreadLocalPage.set(ps);
+                            ps.initRequestVars();
+                            hibSession = utils.HibernateUtil.getCurrentSession();
+                            hibSession.beginTransaction();
+                            if (GlobalVariables.initGlobalVars(ps.envGlobalAndSession,
+                                    utils.HibernateUtil.getCurrentSession())) {
+                                java.io.PrintWriter out = new java.io.PrintWriter(System.out);
+                                ThreadLocalOut.push(out);
+                                webdsl.generated.functions.updateDerivationsAsyncThread_
+                                        .updateDerivationsAsyncThread_(thisThread);
+                                utils.HibernateUtil.getCurrentSession().getTransaction().commit();
+                                ThreadLocalOut.popChecked(out);
+                                ps.invalidatePageCacheIfNeeded();
+                            }
+                        } catch (org.hibernate.StaleStateException
+                                | org.hibernate.exception.LockAcquisitionException ex) {
+                            org.webdsl.logging.Logger
+                                    .error("updateDerivationsAsync() database collision, rescheduling");
+
+                            Settings.reschedule(thisThread);
+
+                            utils.HibernateUtil.getCurrentSession().getTransaction().rollback();
+                        } catch (Exception ex) {
+                            org.webdsl.logging.Logger.error(ex.getClass().getCanonicalName());
+                            org.webdsl.logging.Logger.error("exception occured while executing timed function: "
+                                    + "invoke updateDerivationsAsync() every x milliseconds");
+                            org.webdsl.logging.Logger.error("exception message: " + ex.getMessage(), ex);
+                            utils.HibernateUtil.getCurrentSession().getTransaction().rollback();
+                        } finally {
+                            org.webdsl.servlet.ServletState.scheduledTaskEnded();
+                            ThreadLocalPage.set(null);
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    private void removeWorker(int index) {
+        if (schedules.get(index) != null) {
+            System.out.println("Removing Worker " + index);
+            schedules.get(index).cancel(false);
+            schedules.remove(index);
+        }
+    }
+
+    public void shutdown() {
+        setNumWorkers(0);
+        ex.shutdown();
+    }
 }
