dadce13120bd8f60b2cbd6fa64da1246198f9773
Daco Harkes <dc.harkes@gmail.com>
2018-08-28 14:28:20 +0200

    change version from `0.8.3-SNAPSHOT` to `1.0.0` script

--- change_version_to_stable.sh
+++ change_version_to_stable.sh
@@ -1,5 +1,5 @@
-FROMVERSION=0.8.2
-TOVERSION=0.8.2
+FROMVERSION=0.8.3
+TOVERSION=1.0.0
 
 grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "${FROMVERSION}.qualifier" * | xargs sed -i "" "s/${FROMVERSION}.qualifier/${TOVERSION}/g"
 grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "${FROMVERSION}-SNAPSHOT" * | xargs sed -i "" "s/${FROMVERSION}-SNAPSHOT/${TOVERSION}/g"
