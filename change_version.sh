e3b949a9197ab90547f49c991c0016b8566eb219
Daco Harkes <dc.harkes@gmail.com>
2018-08-28 14:58:05 +0200

    change version from `0.8.3-SNAPSHOT` to `1.0.1-SNAPSHOT` script

--- change_version.sh
+++ change_version.sh
@@ -1,4 +1,4 @@
-FROMVERSION=0.8.2
-TOVERSION=0.8.3
+FROMVERSION=0.8.3
+TOVERSION=1.0.1
 
 grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "${FROMVERSION}(.qualifier|-SNAPSHOT)" * | xargs sed -i "" "s/${FROMVERSION}/${TOVERSION}/g"
