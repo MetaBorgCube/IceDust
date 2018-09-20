5907a853d40fb857231492c8f2e2113fa47bfa09
Daco Harkes <dc.harkes@gmail.com>
2018-09-20 16:03:31 +0200

    change Spoofax version from 2.5.0-SNAPSHOT to 2.6.0-SNAPSHOT script

--- change_spoofax_version.sh
+++ change_spoofax_version.sh
@@ -1,4 +1,4 @@
-FROMVERSION=2.4.0
-TOVERSION=2.5.0
+FROMVERSION=2.5.0
+TOVERSION=2.6.0
 
 grep -rEl --exclude=*/target/* --exclude=*/src-gen/* --include=*.{yaml,xml,MF} "${FROMVERSION}" . | xargs sed -i "" "s/${FROMVERSION}/${TOVERSION}/g"
